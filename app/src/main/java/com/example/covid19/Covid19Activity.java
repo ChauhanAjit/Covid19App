5dbd96263c0bb61cde9844b1c4be7d9a1f75765d
AjitSinghChauhan-SAI <ajitsinghchauhan88@gmail.com>
2020-06-11 16:50:17 +0530

    UI Improved(When api has no data do display or listView is empty then No stats to display message is shown

--- app/src/main/java/com/example/covid19/Covid19Activity.java
+++ app/src/main/java/com/example/covid19/Covid19Activity.java
@@ -7,6 +7,7 @@ import android.content.Loader;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,7 @@ import java.util.List;
 
 public class Covid19Activity extends AppCompatActivity implements LoaderCallbacks<List<CovidStats>> {
 
+    private TextView mEmptyStateTextView;
 
 
     private static final String LOG_TAG = Covid19Activity.class.getName();
@@ -34,30 +36,18 @@ public class Covid19Activity extends AppCompatActivity implements LoaderCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
 
         ListView ListView = (ListView) findViewById(R.id.list);
-
-
+        ListView.setEmptyView(mEmptyStateTextView);
 
         mAdapter = new Covid_Adapter(this, new ArrayList<CovidStats>());
 
-
         ListView.setAdapter(mAdapter);
 
+        LoaderManager loaderManager = getLoaderManager();
 
-
-
-
-
-
-
-            LoaderManager loaderManager = getLoaderManager();
-
-
-            loaderManager.initLoader(COVID_LOADER_ID, null, this);
+        loaderManager.initLoader(COVID_LOADER_ID, null, this);
 
     }
 
@@ -75,6 +65,11 @@ public class Covid19Activity extends AppCompatActivity implements LoaderCallback
         // Hide loading indicator because the data has been loaded
         View loadingIndicator = findViewById(R.id.loading_indicator);
         loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No stats found."
+        mEmptyStateTextView.setText(R.string.no_new_stats);
+
+
         mAdapter.clear();
         if (data != null && !data.isEmpty()) {
             mAdapter.addAll(data);
